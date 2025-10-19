ff005418f2a527a7cbbe600c848c3324bec61520
EL Houssine <136338273+houssine111111@users.noreply.github.com>
2025-10-19 16:38:14 +0100

    Refactor imports in bookRoutes.js

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -1,7 +1,8 @@
-import express from "express";
-import cloudinary from "../lib/cloudinary.js";
-import Book from "../models/Book.js";
-import protectRoute from "../middleware/auth.middleware.js";
+import express from 'express';
+import Book from '../modules/Book.js';
+import jwt from 'jsonwebtoken';
+import cloudinary from '../lib/cloudinary.js';
+import { protectRoute } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
 
